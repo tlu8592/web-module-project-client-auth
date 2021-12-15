8e38ef27a3f22cf6e9bf340ce1cde58000d2ccba
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-14 19:49:43 -0500

    refactored navlinks to react router syntax

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Login from './Login';
+
+const navLinkStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    textDecoration: 'none',
+    fontSize: '2rem',
+    fontWeight: '700',
+    padding: '2rem 2rem',
+    paddingTop: '2.5rem'
+}
 
 const NavBar = () => {
     return (
@@ -6,10 +18,16 @@ const NavBar = () => {
             <div className="header">
                 <h1>Friends Database</h1>
                 <nav className="navbar">
-                    <a href="#">Login</a>
+                    {/* <a href="#">Login</a>
                     <a href="#">FriendList</a>
                     <a href="#">AddFriend</a>
-                    <a href="#">Logout</a>
+                    <a href="#">Logout</a> */}
+                    <Link to='/login' style={navLinkStyle}>
+                        Login
+                    </Link>
+                    <Link to={null} style={navLinkStyle}>FriendList</Link>
+                    <Link to={null} style={navLinkStyle}>AddFriend</Link>
+                    <Link to={null} style={navLinkStyle}>Logout</Link>
                 </nav>
             </div>
             <hr />
